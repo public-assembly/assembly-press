9ce46f07065b903177b09dd71c773424f2f7a317
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-30 11:13:59 -0300

    I add passing unit tests for AdminAccess.

--- tokenized-access-control/test/Erc20AccessControl.t.sol
+++ tokenized-access-control/test/Erc20AccessControl.t.sol
@@ -101,4 +101,40 @@ contract Erc20AccessControlTest is DSTest {
         assertTrue(mockCurator.managerAccessTest());
         assertTrue(!mockCurator.adminAccessTest());
     }
+
+    function test_AdminAccess() public {
+        vm.startPrank(DEFAULT_OWNER_ADDRESS);
+        uint256 tokenBalance = 1 ether;
+        erc20Admin.mint(DEFAULT_OWNER_ADDRESS, tokenBalance);
+        Erc20AccessControl e20AccessControl = new Erc20AccessControl();
+
+        MockCurator mockCurator = new MockCurator();
+        mockCurator.initializeAccessControl(
+            address(e20AccessControl),
+            address(erc20Curator),
+            address(erc20Manager),
+            address(erc20Admin)
+        );
+        assertTrue(
+            mockCurator.accessControlProxy() == address(e20AccessControl)
+        );
+        assertTrue(mockCurator.getAccessLevelForUser() == 3);
+        assertTrue(mockCurator.curatorAccessTest());
+        assertTrue(mockCurator.managerAccessTest());
+        assertTrue(mockCurator.adminAccessTest());
+
+        erc20Admin.transfer(DEFAULT_NON_OWNER_ADDRESS, tokenBalance);
+        assertTrue(mockCurator.getAccessLevelForUser() == 0);
+        assertTrue(!mockCurator.curatorAccessTest());
+        assertTrue(!mockCurator.managerAccessTest());
+        assertTrue(!mockCurator.adminAccessTest());
+
+        vm.stopPrank();
+        vm.startPrank(DEFAULT_NON_OWNER_ADDRESS);
+
+        assertTrue(mockCurator.getAccessLevelForUser() == 3);
+        assertTrue(mockCurator.curatorAccessTest());
+        assertTrue(mockCurator.managerAccessTest());
+        assertTrue(mockCurator.adminAccessTest());
+    }
 }
