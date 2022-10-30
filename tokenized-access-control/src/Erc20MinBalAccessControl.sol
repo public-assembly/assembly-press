99f490f3316a01586eb9b931d8f489865fa5592a
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-30 17:19:44 -0300

    I update natspec comments to ref ERC20 instead of ERC721.

--- tokenized-access-control/src/Erc20MinBalAccessControl.sol
+++ tokenized-access-control/src/Erc20MinBalAccessControl.sol
@@ -79,7 +79,7 @@ contract Erc20MinBalAccessControl is IAccessControlRegistry {
     // WRITE FUNCTIONS
     //////////////////////////////////////////////////
 
-    /// @notice updates ERC721 address used to define curator access
+    /// @notice updates ERC20 address used to define curator access
     function updateCurator(
         address target,
         IERC20 newCuratorAccess,
@@ -99,7 +99,7 @@ contract Erc20MinBalAccessControl is IAccessControlRegistry {
         });
     }
 
-    /// @notice updates ERC721 address used to define manager access
+    /// @notice updates ERC20 address used to define manager access
     function updateManagerAccess(
         address target,
         IERC20 newManagerAccess,
@@ -119,7 +119,7 @@ contract Erc20MinBalAccessControl is IAccessControlRegistry {
         });
     }
 
-    /// @notice updates ERC721 address used to define admin access
+    /// @notice updates ERC20 address used to define admin access
     function updateAdminAccess(
         address target,
         IERC20 newAdminAccess,
@@ -139,7 +139,7 @@ contract Erc20MinBalAccessControl is IAccessControlRegistry {
         });
     }
 
-    /// @notice updates ERC721 address used to define curator, manager, and admin access
+    /// @notice updates ERC20 address used to define curator, manager, and admin access
     function updateAllAccess(
         address target,
         IERC20 newCuratorAccess,
@@ -172,7 +172,7 @@ contract Erc20MinBalAccessControl is IAccessControlRegistry {
     }
 
     /// @notice initializes mapping of token roles
-    /// @dev contract getting access control => erc721 addresses used for access control of different roles
+    /// @dev contract getting access control => ERC20 addresses used for access control of different roles
     /// @dev called by other contracts initiating access control
     /// @dev data format: curatorAccess, managerAccess, adminAccess
     function initializeWithData(bytes memory data) external {
@@ -244,7 +244,7 @@ contract Erc20MinBalAccessControl is IAccessControlRegistry {
         return accessMapping[addressToCheck];
     }
 
-    /// @notice returns the erc721 address being used for curator access control
+    /// @notice returns the ERC20 address being used for curator access control
     function getCuratorInfo(address addressToCheck)
         external
         view
@@ -253,7 +253,7 @@ contract Erc20MinBalAccessControl is IAccessControlRegistry {
         return accessMapping[addressToCheck].curatorAccess;
     }
 
-    /// @notice returns the erc721 address being used for manager access control
+    /// @notice returns the ERC20 address being used for manager access control
     function getManagerInfo(address addressToCheck)
         external
         view
@@ -262,7 +262,7 @@ contract Erc20MinBalAccessControl is IAccessControlRegistry {
         return accessMapping[addressToCheck].managerAccess;
     }
 
-    /// @notice returns the erc721 address being used for admin access control
+    /// @notice returns the ERC20 address being used for admin access control
     function getAdminInfo(address addressToCheck)
         external
         view
