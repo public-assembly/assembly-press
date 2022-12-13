2b68d3bcf80c0c7b9728d2ea491206f2592103b3
Salief Lewis <salieflewis@gmail.com>
2022-12-13 10:34:35 -0500

    constructor becomes initialize function

--- src/AssemblyPress.sol
+++ src/AssemblyPress.sol
@@ -18,35 +18,30 @@ import {PublisherStorage} from "./Publisher.sol";
  * @author Max Bochman
  *
  */
-contract AssemblyPress is
-    OwnableUpgradeable, 
-    ReentrancyGuardUpgradeable, 
-    PublisherStorage  
-{
-
+contract AssemblyPress is OwnableUpgradeable, ReentrancyGuardUpgradeable, PublisherStorage {
     // ||||||||||||||||||||||||||||||||
     // ||| ERRORS |||||||||||||||||||||
-    // ||||||||||||||||||||||||||||||||     
+    // ||||||||||||||||||||||||||||||||
 
     error CantSet_ZeroAddress();
 
     // ||||||||||||||||||||||||||||||||
     // ||| EVENTS |||||||||||||||||||||
     // ||||||||||||||||||||||||||||||||
- 
+
     // constructor events
-    event ZoraProxyAddressInitialized(address zoraProxyAddress); 
-    event ZEditionMetadataRendererInitialized(address zoraProxyAddress); 
-    event PublisherInitialized(address publisherAddress); 
+    event ZoraProxyAddressInitialized(address zoraProxyAddress);
+    event ZEditionMetadataRendererInitialized(address zoraProxyAddress);
+    event PublisherInitialized(address publisherAddress);
 
     // event called when base impl addresses updated
-    event ZoraProxyAddressUpdated(address sender, address newZoraProxyAddress);     
-    event ZEditionMetadataRendererUpdated(address sender, address newZEditionMetadataRendererAddress);     
-    event PublisherUpdated(address sender, address newPublisherAddress);     
+    event ZoraProxyAddressUpdated(address sender, address newZoraProxyAddress);
+    event ZEditionMetadataRendererUpdated(address sender, address newZEditionMetadataRendererAddress);
+    event PublisherUpdated(address sender, address newPublisherAddress);
 
     // ||||||||||||||||||||||||||||||||
     // ||| STORAGE ||||||||||||||||||||
-    // ||||||||||||||||||||||||||||||||     
+    // ||||||||||||||||||||||||||||||||
 
     bytes32 public immutable DEFAULT_ADMIN_ROLE = 0x00;
     address public zoraNFTCreatorProxy;
@@ -54,14 +49,12 @@ contract AssemblyPress is
     Publisher public publisher;
 
     // ||||||||||||||||||||||||||||||||
-    // ||| CONSTRUCTOR ||||||||||||||||
-    // ||||||||||||||||||||||||||||||||  
+    // ||| INITIALIZER ||||||||||||||||
+    // ||||||||||||||||||||||||||||||||
 
-    constructor(
-        address _zoraNFTCreatorProxy, 
-        address _zEditionMetadataRenderer,
-        Publisher _publisher
-    ) {
+    function initialize(address _zoraNFTCreatorProxy, address _zEditionMetadataRenderer, Publisher _publisher)
+        initializer
+    {
         zoraNFTCreatorProxy = _zoraNFTCreatorProxy;
         zEditionMetadataRenderer = _zEditionMetadataRenderer;
         publisher = _publisher;
@@ -73,7 +66,7 @@ contract AssemblyPress is
 
     // ||||||||||||||||||||||||||||||||
     // ||| createPublication ||||||||||
-    // |||||||||||||||||||||||||||||||| 
+    // ||||||||||||||||||||||||||||||||
 
     function createPublication(
         string memory name,
@@ -88,9 +81,8 @@ contract AssemblyPress is
         bytes memory accessControlInit,
         uint256 mintPricePerToken
     ) public nonReentrant returns (address) {
-
         // encode contractURI + mintPricePerToken + accessControl + accessControl init to pass into ArtifactMachineRegistry
-        bytes memory publisherInitializer = abi.encode(contractURI, mintPricePerToken, accessControl, accessControlInit); 
+        bytes memory publisherInitializer = abi.encode(contractURI, mintPricePerToken, accessControl, accessControlInit);
 
         // deploy zora collection - defaultAdmin must be address(this) here but will be updated later
         address newDropAddress = IZoraCreatorInterface(zoraNFTCreatorProxy).setupDropsContract(
@@ -102,20 +94,20 @@ contract AssemblyPress is
             fundsRecipient,
             saleConfig,
             publisher,
-            publisherInitializer           
+            publisherInitializer
         );
 
         // give publisher minter role on zora drop
         ERC721Drop(payable(newDropAddress)).grantRole(MINTER_ROLE, address(publisher));
 
         // grant admin role to desired admin address
-        ERC721Drop(payable(newDropAddress)).grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);    
+        ERC721Drop(payable(newDropAddress)).grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
 
         // revoke admin role from address(this) as it differed from desired admin address
         ERC721Drop(payable(newDropAddress)).revokeRole(DEFAULT_ADMIN_ROLE, address(this));
 
         return newDropAddress;
-    }    
+    }
 
     // ||||||||||||||||||||||||||||||||
     // ||| ADMIN FUNCTIONS ||||||||||||
@@ -124,7 +116,6 @@ contract AssemblyPress is
     /// @dev updates address value of zoraNFTCreatorProxy
     /// @param newZoraNFTCreatorProxy new zoraNFTCreatorProxy address
     function setZoraCreatorProxyAddress(address newZoraNFTCreatorProxy) public onlyOwner {
-
         if (newZoraNFTCreatorProxy == address(0)) {
             revert CantSet_ZeroAddress();
         }
@@ -132,12 +123,11 @@ contract AssemblyPress is
         zoraNFTCreatorProxy = newZoraNFTCreatorProxy;
 
         emit ZoraProxyAddressUpdated(msg.sender, newZoraNFTCreatorProxy);
-    }      
+    }
 
     /// @dev updates address value of publisher
     /// @param newPublisher new newPublisher address
     function setPublisher(Publisher newPublisher) public onlyOwner {
-
         if (address(newPublisher) == address(0)) {
             revert CantSet_ZeroAddress();
         }
@@ -145,12 +135,11 @@ contract AssemblyPress is
         publisher = newPublisher;
 
         emit PublisherUpdated(msg.sender, address(newPublisher));
-    }      
+    }
 
     /// @dev updates address value of zEditionMetadataRenderer
     /// @param newZEditionMetadataRenderer new ZEditionMetadataRenderer address
     function setzEditionMetadataRenderer(address newZEditionMetadataRenderer) public onlyOwner {
-
         if (newZEditionMetadataRenderer == address(0)) {
             revert CantSet_ZeroAddress();
         }
@@ -158,40 +147,40 @@ contract AssemblyPress is
         zEditionMetadataRenderer = newZEditionMetadataRenderer;
 
         emit ZEditionMetadataRendererUpdated(msg.sender, newZEditionMetadataRenderer);
-    }             
+    }
 }
 
-    // function promoteToEdition(
-    //     address zoraDrop,
-    //     uint256 tokenId,        
-    //     string memory name,
-    //     string memory symbol,
-    //     uint64 editionSize,
-    //     uint16 royaltyBPS,
-    //     address payable fundsRecipient,
-    //     address defaultAdmin,        
-    //     IERC721Drop.SalesConfiguration memory saleConfig,
-    //     string memory description
-    // ) public nonReentrant returns (address) {
+// function promoteToEdition(
+//     address zoraDrop,
+//     uint256 tokenId,
+//     string memory name,
+//     string memory symbol,
+//     uint64 editionSize,
+//     uint16 royaltyBPS,
+//     address payable fundsRecipient,
+//     address defaultAdmin,
+//     IERC721Drop.SalesConfiguration memory saleConfig,
+//     string memory description
+// ) public nonReentrant returns (address) {
 
-    //     // check if msg.sender has publication access
-    //     if (IAccessControlRegistry(dropAccessControl[zoraDrop]).getAccessLevel(address(this), msg.sender) < 1) {
-    //         revert No_PublicationAccess();
-    //     }                 
+//     // check if msg.sender has publication access
+//     if (IAccessControlRegistry(dropAccessControl[zoraDrop]).getAccessLevel(address(this), msg.sender) < 1) {
+//         revert No_PublicationAccess();
+//     }
 
-    //     // deploy zora collection that pulls info from PublisherStorage
-    //     address newDropAddress = IZoraCreatorInterface(zoraNFTCreatorProxy).createEdition(
-    //         name,
-    //         symbol,
-    //         editionSize,
-    //         royaltyBPS,
-    //         fundsRecipient,
-    //         defaultAdmin,
-    //         saleConfig,
-    //         description,
-    //         zoraDrop.tokenURI(tokenId),
-    //         zoraDrop.tokenURI(tokenId)
-    //     );
+//     // deploy zora collection that pulls info from PublisherStorage
+//     address newDropAddress = IZoraCreatorInterface(zoraNFTCreatorProxy).createEdition(
+//         name,
+//         symbol,
+//         editionSize,
+//         royaltyBPS,
+//         fundsRecipient,
+//         defaultAdmin,
+//         saleConfig,
+//         description,
+//         zoraDrop.tokenURI(tokenId),
+//         zoraDrop.tokenURI(tokenId)
+//     );
 
-    //     return newDropAddress;
-    // }
+//     return newDropAddress;
+// }
