3ad8e85f86dd56caf582e18fd1030ea58a4bdc82
Salief Lewis <salieflewis@gmail.com>
2022-12-15 16:35:59 -0500

    update import in access control arch script

--- script/AccessControlArch.s.sol
+++ script/AccessControlArch.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import {OnlyAdminAccessControl} from "onchain/OnlyAdminAccessControl.sol";
+import {OnlyAdminAccessControl} from "onchain/remote-access-control/src/OnlyAdminAccessControl.sol";
 
 contract DeployCore is Script {
 
