f0e543dcf9d31e2ea0e1b40ecdaafa6f0f4cb5a5
0xTranqui <thinkandchill11@gmail.com>
2022-12-15 11:50:25 -0600

    manaully removed onchain repo

--- lib/onchain/zora-tiered-pricing-minter/lib/nft-drops-extensions/src/nouns-vision/ERC721OwnerInterface.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-
-interface ERC721OwnerInterface {
-  function ownerOf(uint256 tokenid) external returns (address);
-  function isApprovedForAll(address owner, address operator) external returns (bool);
-  function burn(uint256 tokenId) external;
-}
