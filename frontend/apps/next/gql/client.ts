f42359aa3671042b6a6ea3aeb750fe2722db5aaa
jawndiego <jawndiego@gmail.com>
2023-08-10 11:05:51 -0400

    cleaned client

--- frontend/apps/next/gql/client.ts
+++ frontend/apps/next/gql/client.ts
@@ -4,7 +4,6 @@ import { getSdk } from './sdk.generated'
 const client = new GraphQLClient('https://striking-possum-76.hasura.app/v1/graphql', {
   headers: {
     'Content-Type': 'application/json',
-    'x-hasura-admin-secret': 'mI8pBnqi8ojlMcpfDF8bxrAr5HbWh4uNv5UZLb0lXnWfNqDFRzmuF7q85A63lof8'
   },
 })
 
