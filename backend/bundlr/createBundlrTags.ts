44e28e927ad8cae820abc006cda2fa88cd66c1cc
Salief <salieflewis@gmail.com>
2023-08-21 08:43:20 -0400

    db events becomes router events for bundlr metadata

--- backend/bundlr/createBundlrTags.ts
+++ backend/bundlr/createBundlrTags.ts
@@ -2,7 +2,7 @@ export const createBundlrTags = (tableName: string) => {
   const tags = [
     { name: 'Content-Type', value: 'application/json' },
     {
-      name: `Database Events - ${process.env.CHAIN_ID} v0.1`,
+      name: `Router Events - ${process.env.CHAIN_ID} v0.1`,
       value: `${tableName}`,
     },
   ]
