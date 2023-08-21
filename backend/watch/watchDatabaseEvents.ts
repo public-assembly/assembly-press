a01c51ee57ac8830b2937a84dc51fb0a9aa3fbae
Salief <salieflewis@gmail.com>
2023-08-21 08:42:48 -0400

    change node1 to node2 in prod

--- backend/watch/watchDatabaseEvents.ts
+++ backend/watch/watchDatabaseEvents.ts
@@ -1,14 +1,13 @@
 import { parseAbi, type Hex } from 'viem'
 import { decodeLogs } from '../transform/decodeLogs'
 import { viemClient } from '../viem/client'
-import { databaseAbiEventsArray } from '../constants/events'
+import { routerAbiEventsArray } from '../constants'
 import { writeToDatabase } from '../prisma'
 
-
 export const watchDatabaseEvents = () => {
-    const parsedEvent = parseAbi(databaseAbiEventsArray)
+    const parsedEvent = parseAbi(routerAbiEventsArray)
     viemClient.watchEvent({
-      address: process.env.DATABASE_ADDRESS as Hex,
+      address: process.env.ROUTER_ADDRESS as Hex,
       events: parsedEvent,
       onLogs: (logs) => {
         writeToDatabase(decodeLogs(logs))
