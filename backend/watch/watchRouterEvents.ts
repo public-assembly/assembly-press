18072f51df940a89137068e54783b02d8047071e
Salief <salieflewis@gmail.com>
2023-08-21 18:50:36 -0400

    rename watch fn

--- backend/watch/watchRouterEvents.ts
+++ backend/watch/watchRouterEvents.ts
@@ -1,18 +1,18 @@
-import { parseAbi, type Hex } from 'viem'
-import { decodeLogs } from '../transform/decodeLogs'
-import { viemClient } from '../viem/client'
-import { routerAbiEventsArray } from '../constants'
-import { writeToDatabase } from '../prisma'
+import { parseAbi, type Hex } from 'viem';
+import { decodeLogs } from '../transform/decodeLogs';
+import { viemClient } from '../viem/client';
+import { routerAbiEventsArray } from '../constants';
+import { writeToDatabase } from '../prisma';
 
 export const watchRouterEvents = () => {
-    const parsedEvent = parseAbi(routerAbiEventsArray)
-    console.log('Watching router events...')
-    viemClient.watchEvent({
-      address: process.env.ROUTER_ADDRESS as Hex,
-      events: parsedEvent,
-      onLogs: (logs) => {
-        console.log(decodeLogs(logs))
-        // writeToDatabase(decodeLogs(logs))
-      }
-    })
-  }
+  const parsedEvent = parseAbi(routerAbiEventsArray);
+  console.log('Watching router events...');
+  viemClient.watchEvent({
+    address: process.env.ROUTER_ADDRESS as Hex,
+    events: parsedEvent,
+    onLogs: (logs) => {
+      console.log(decodeLogs(logs));
+      // writeToDatabase(decodeLogs(logs))
+    },
+  });
+};
