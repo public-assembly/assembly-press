0420ada6143d7a43782149b64c9c1db6f6a43e3f
0xTranqui <thinkandchill11@gmail.com>
2024-01-04 23:05:48 -0500

    item renderer

--- protocol/src/core/press/renderer/ItemRenderer.sol
+++ protocol/src/core/press/renderer/ItemRenderer.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.19;
 
 import "sstore2/SSTORE2.sol";
 import "solidity-bytes-utils/BytesLib.sol";
+import {MetadataBuilder} from "micro-onchain-metadata-utils/MetadataBuilder.sol";
+import {MetadataJSONKeys} from "micro-onchain-metadata-utils/MetadataJSONKeys.sol";
+import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
 
 /**
  * @title ItemRenderer
@@ -17,48 +20,62 @@ contract ItemRenderer {
         NFT
     }
 
-    function decodeUri(address pointer) public returns (string memory uri) {
-        (uint16 dataType, bytes memory data)  = extractType(SSTORE2.read(pointer));
+    function decodeUri(address pointer) public view returns (string memory uri) {
+        (uint16 dataType, bytes memory data) = extractType(SSTORE2.read(pointer));
         if (dataType > uint16(DataTypes.NFT)) revert Invalid_Data_Type();
-        if (dataType == uint16(DataTypes.STRING)) return decodeString()
-
+        if (dataType == uint16(DataTypes.STRING)) return decodeString(data);
+        if (dataType == uint16(DataTypes.NFT)) return decodeNft(data);
     }
 
-    function extractType(bytes memory data) public pure returns (uint16 dataType, bytes memory data) {
+    function extractType(bytes memory data) public pure returns (uint16 typeData, bytes memory rawData) {
         uint256 length = data.length;
         // Get data type
         bytes memory rawSliceForType = BytesLib.slice(data, 0, 2);
-        bytes2 cleanedSliceForType = bytes2(rawSlice);        
-        dataType = uint16(cleanedSlice);            
+        bytes2 cleanedSliceForType = bytes2(rawSliceForType);        
+        typeData = uint16(cleanedSliceForType);            
         // Get data
-        data = BytesLib.slice(data, 2, length - 2);          
+        rawData = BytesLib.slice(data, 2, length - 2);          
     }
 
     function decodeString(bytes memory data) public pure returns (string memory str) {
         return string(data);
     }
 
-    function decodeNft(bytes memory data) public pure 
-        returns (
+    function decodeNft(bytes memory data) public pure returns (string memory nft) {
+        (
             uint256 chainId, 
             address tokenContract, 
-            uint256 tokenId, 
+            uint256 tokenId,
             bool hasId
-        ) 
-    {
-        (
-            chainId, 
-            tokenContract, 
-            tokenId,
-            hasId
         ) = abi.decode(data, (uint256, address, uint256, bool));
 
-        return (chainId, tokenContract, tokenId, hasId);
+        MetadataBuilder.JSONItem[] memory items = new MetadataBuilder.JSONItem[](4);
+
+        items[0].key = "chainId";
+        items[0].value = Strings.toString(chainId);
+        items[0].quote = true;
+        items[1].key = "tokenContract";
+        items[1].value = Strings.toHexString(tokenContract);
+        items[1].quote = true;
+        items[2].key = "tokenId";
+        items[2].value = Strings.toString(tokenId);
+        items[2].quote = true;        
+        items[3].key = "hasId";
+        items[3].value = hasTokenIdConverter(hasId);
+        items[3].quote = true;        
+
+        nft = MetadataBuilder.generateEncodedJSON(items);
     }
 
+    // ||||||||||||||||||||||||||||||||
+    // ||| INTERNAL FUNCTIONS |||||||||
+    // ||||||||||||||||||||||||||||||||  
 
-
-
-
+    // helper function for hasTokenId metadata returns
+    function hasTokenIdConverter(bool hasTokenId) internal pure returns (string memory) {
+        if (hasTokenId) {
+            return "true";
+        }
+        return "false";
+    }    
 }
-
